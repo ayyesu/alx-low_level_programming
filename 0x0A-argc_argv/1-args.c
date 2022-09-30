13029a0ef73ea0b80fff04bbeb02a9b9c853cb8e
ayyesu <iamdanielkissiedu@gmail.com>
2022-09-30 07:51:54 -0700

    Added file-02

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program that prints the number of arguments
+ *        passed into it
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	int i = 0, m;
+
+	while (i < argc)
+	{
+		m = i;
+		i++;
+	}
+	printf("%d\n", m);
+	return (0);
+}
