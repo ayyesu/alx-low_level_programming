9615fd039ba4aaa9b8706e470851451452c557df
ayyesu <iamdanielkissiedu@gmail.com>
2022-10-28 02:49:40 -0700

    Correction made

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,78 +1,81 @@
 #include "main.h"
 
-/**
- * _atoi - converts chars to integer
- *
- * @c: char to convert
- *
- * Return: converted integer
-*/
-unsigned int _atoi(char c)
-{
-	return ((unsigned int) c - '0');
-}
 
 /**
- * _strlen - gets the length of a string
- *
- * @str: string input
- *
- * Return: string length
+*_strlen - function to calculate length of a string
+*@b: the string
+*Return: Returns length of the string
 */
-unsigned int _strlen(const char *str)
+
+int _strlen(const char *b)
 {
-	unsigned int index = 0;
+	int i;
 
-	while (str[index] != '\0')
-		index++;
-	return (index);
-}
-
-/**
- * binary_to_uint - a function that converts a binary number
- *                  to an unsigned int
- *
- * @b: string that contains 0 and 1 characters
- *
- * Return: the number converted or 0
- *         if @b contains a character
- *         that is not 0 or 1 or when
- *         @b is null
-*/
-unsigned int binary_to_uint(const char *b)
-{
-	int index;
-	unsigned int result = 0, base2 = 1,  num = 0;
-
-	/*if b is NULL return 0*/
 	if (b == NULL)
 		return (0);
+	for (i = 0; *(b + i) != '\0'; i++)
+		;
+	return (i);
+}
 
-	#ifdef DEBUG /*print debug statement*/
-	printf("String is %s, and length is %u.\n", b, _strlen(b));
-	#endif
 
-	/*iterate through string*/
-	for (index = _strlen(b) - 1; index >= 0; index--)
+
+
+/**
+*_pow - function to calculate number to power
+*@n: the number
+*@p: the power
+*Return: Returns n^p
+*/
+
+int _pow(int n, int p)
+{
+	int r, i;
+
+	r = n;
+	if (n == 0)
+		return (0);
+	else if (p == 0)
+		return (1);
+	for (i = 1; i < p; i++)
+		r *= n;
+	return (r);
+}
+
+
+
+/**
+*binary_to_uint - function that converts binary to decimal
+*@b: pointer to the string of binary number to convert
+*Return: the convert number if successfull or 0 if fail
+*/
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	int len, i;
+	unsigned int num;
+	char c;
+
+	num = 0;
+	if (b == NULL)
+		return (0);
+	len = _strlen(b);
+	for (i = 0; len >= 0; i++)
 	{
-		num = _atoi(b[index]); /*convert char to number*/
-
-		#ifdef DEBUG /*print debug statements*/
-		printf("Number is %u, index is %i and base is %u.\n\n", num, index, base2);
-		#endif
-
-		/*if number is not 0 or 1 return 0*/
-		if (num != 0 && num != 1)
+		c = *(b + i);
+		if (c == '\0')
+			break;
+		if (c != '0' && c != '1')
 			return (0);
-
-		result += num * base2; /*enable debug to see it in action*/
-		base2 *= 2;
-
-		#ifdef DEBUG /*print debug statements*/
-		printf("Result is %u.\n", result);
-		#endif
+		if (c == '1')
+		{
+			num += (1 * _pow(2, --len));
+		}
+		else
+		{
+			--len;
+		}
 	}
-
-	return (result);
-
+	return (num);
 }
